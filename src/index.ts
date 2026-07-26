8341d240f001f08ee3a4d87b5e54df6e66690f57
Youssof Barazi <youssof.barazi02@gmail.com>
2026-07-26 10:13:17 +0300

    Feat: Implement user registration, login, and JWT auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,17 @@
 // src/index.ts
 import express from 'express';
 import cors from 'cors';
-import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
+import authRoutes from './routes/auth.route.js'
 
 const app = express();
-
-// 1. Initialize the standard Postgres Pool using your pooled Supabase URL
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({ connectionString });
-
-// 2. Wrap it in the Prisma Adapter
-const adapter = new PrismaPg(pool);
-
-// 3. Pass the adapter to the PrismaClient constructor
-const prisma = new PrismaClient({ adapter });
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/health', async (req, res) => {
-  try {
-    // You can now query your database perfectly!
-    res.json({ status: 'success', message: 'السيرفر يعمل بكفاءة!' });
-  } catch (error) {
-    res.status(500).json({ status: 'error', message: 'خطأ داخلي' });
-  }
-});
+// Auth Endpoints
+app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
